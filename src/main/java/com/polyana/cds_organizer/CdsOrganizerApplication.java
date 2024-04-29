e15b4155605c8c7d5e4feb9c66d9c0b860c01461
polyana <polygaspar2000@gmail.com>
2024-04-29 19:44:38 -0300

    Spring Boot API for CD Organizer Project

---
+++ src/main/java/com/polyana/cds_organizer/CdsOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.polyana.cds_organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdsOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdsOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polyana/cds_organizer/CdsOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polyana.cds_organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdsOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

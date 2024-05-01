b084867dd1ced943cfa9e9990249e88595b19b26
polyana <polygaspar2000@gmail.com>
2024-05-01 10:42:25 -0300

    Create UserService

---
+++ src/main/java/com/polyana/cds_organizer/services/UserService.java
@@ -0,0 +1,54 @@
+package com.polyana.cds_organizer.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.polyana.cds_organizer.models.User;
+import com.polyana.cds_organizer.repositories.CdsRepository;
+import com.polyana.cds_organizer.repositories.UserRepository;
+
+@Service
+
+public class UserService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private CdsRepository cdsRepository;
+
+  public User findById(Long id) {
+    Optional<User> user = this.userRepository.findById(id);
+    return user.orElseThrow(() -> new RuntimeException(
+      "Usuário não encontrado!"
+    ));
+  }
+
+  @Transactional
+  public User create(User obj) {
+    obj.setId(null);
+    obj = this.userRepository.save(obj);
+    this.cdsRepository.saveAll(obj.getCds());
+    return obj;
+  }
+
+  @Transactional
+  public User update(User obj) {
+    User newObj = findById(obj.getId());
+    newObj.setPassword(obj.getPassword());
+    return this.userRepository.save(newObj);
+  }
+
+  public void delete(Long id) {
+    findById(id);
+    try {
+      this.userRepository.deleteById(id);
+    } catch (Exception e) {
+      throw new RuntimeException("Não é possível excluir o usuário!")
+    }
+  }
+
+}
